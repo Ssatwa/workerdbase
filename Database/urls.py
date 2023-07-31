56a4d9c9c7168520497a5b8bfd57ed596852a61e
Drill <140717616+drillmoid@users.noreply.github.com>
2023-08-01 00:33:52 +0300

    Bug Report

--- Database/urls.py
+++ Database/urls.py
@@ -6,7 +6,7 @@ from Database import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', views.index),
-    path('show', views.show),
+    path('show/', views.show),
     path('edit/<int:id>', views.edit),
     path('delete/<int:id>', views.delete),
     path('update/<int:id>', views.update)
